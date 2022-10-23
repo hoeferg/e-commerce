270ff8801d3ef3735bf257fdbf98eae52396a170
Gayle Hoefer <gaylehoefer@gmail.com>
2022-10-23 16:05:57 -0700

    finished index.js and product.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,10 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Produce.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+})
 
 // Tags belongToMany Products (through ProductTag)
 
