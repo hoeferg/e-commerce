599031360f36fa3e5ccbdc487be8d0f03e4ed306
Gayle Hoefer <gaylehoefer@gmail.com>
2022-10-23 19:02:48 -0700

    finished category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,24 +6,37 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  const catagoriesData = await User.findAll(
-    // include: {
-    //   model: Product,
-    //   attributes: {'product_name'},
-    // }
-  ).catch((err) => {
+  const catagoriesData = await Product.findAll({
+    include: {
+      model: Product,
+      attributes: ['product_name'],
+    }
+  }).catch((err) => {
     res.json(err);
   });
   res.json(catagoriesData);
 });
+
+router.get('/', async (req, res) => {
 // find one category by its `id` value
 // be sure to include its associated Products
+try {
+  const catagoriesData = await Product.findByPk(req.params.id);
+  if (!catagoriesData) {
+    res.status(404).json({ message: 'No product with this id!' });
+    return;
+  }
+  res.status(200).json(catagoriesData);
+} catch (err) {
+  res.status(500).json(err);
+}
+});
 
 
-router.post('/', async (req, res) => {
+router.post('/:id', async (req, res) => {
   // create a new category
   try {
-    const catagoriesData = await User.create(req.body);
+    const catagoriesData = await Product.create(req.body);
     // 200 status code means the request is successful
     res.status(200).json(catagoriesData);
   } catch (err) {
@@ -37,13 +50,13 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const catagoriesData = await User.update(req.body, {
+    const catagoriesData = await Product.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
     if (!catagoriesData[0]) {
-      res.status(404).json({ message: 'No item with this id!' });
+      res.status(404).json({ message: 'No product with this id!' });
       return;
     }
     res.status(200).json(catagoriesData);
@@ -56,13 +69,13 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const userData = await User.destroy({
+    const userData = await Product.destroy({
       where: {
         id: req.params.id,
       },
     });
     if (!userData) {
-      res.status(404).json({ message: 'No user with this id!' });
+      res.status(404).json({ message: 'No product with this id!' });
       return;
     }
     res.status(200).json(userData);
