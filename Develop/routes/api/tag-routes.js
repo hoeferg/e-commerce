e06cc277e29336ac577bd4017ab3485cdd5ef542
Gayle Hoefer <gaylehoefer@gmail.com>
2022-10-24 21:21:58 -0700

    fixed tag-routes put

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -55,7 +55,11 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(req.params.id);
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id:req.params.id,
+      },
+    });
     if (!tagData) {
       res.status(404).json({ message: 'No Product with this id!' });
       return;
